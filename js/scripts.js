3ee69aa3255ea325517a3444df8d015d38df09b1
Yvonne-Ouma <yvonneouma98@gmail.com>
2018-07-22 23:30:19 +0300

    modified my directory

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
-
+// BUSINESS LOGIC
 function rollDice() {
-  var die1 = document.getElementById("die1");
-  var status = document.getElementById("status");
-  var d1 = Math.floor(Math.random() * 6) + 1;
-  var diceTotal = d1 ;
-  die1.innerHTML = d1;
-  status.innerHTML ="You rolled "+ d1 +".";
+  var roll1 = document.getElementById("roll1");
+  var where = document.getElementById("where");
+  var r1 = Math.floor(Math.random() * 6) + 1;
+  var diceTotal = r1 ;
+  roll1.innerHTML = r1;
+  where.innerHTML ="You rolled "+ r1 +".";
   if(d1 == 6) {
-    status.innerHTML += " DOUBLES! You get free turn! !";
+    where.innerHTML += " DOUBLES! You get free turn! !";
   }
 }
 
@@ -19,14 +19,14 @@ function throwDice() {
   var diceTotal = p2;
   play2.innerHTML = p2;
   state.innerHTML ="You rolled "+ diceTotal +".";
-  if(p1 == 3) {
-    state.innerHTML += " You got it";
+  if(p2 == 3) {
+    state.innerHTML += " Wow! You got it! !";
   }
 }
-
+// USER INTERFACE LOGIC
 $(document).ready(function(){
   $("button#start").click(function(event){
-   die1 = new d1;
-   play2 = new p2;
+   roll1 = new d1 ;
+   play2 = new p2 ;
   })
 })
